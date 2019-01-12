a1170a5ced510fcbd8f6e7b54ca0a399d2ae026b
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-01-12 18:28:56 -0300

    criação do controle de entrada e saida de usuario (login e logout)

--- core/views.py
+++ core/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render, redirect
 from core.forms import *
 from core.models import *
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    # Login
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        senha = request.POST.get('senha')
+        if logar(request, login, senha):
+            if recuperar_elemento_da_session(request, 'tipo_de_usuario') == 'confeiteiro':
+                return redirect('/admin')
+            else:
+                return redirect('/admin')
+
+    # dados que serão enviados ao template
+    dicionario = {
+        'confeiteiros': seleciona_todos_os_confeiteiros_cadastrados(),
+        'usuario_logado': recuperar_elemento_da_session(request, 'usuario_logado'),
+        'nome_do_usuario': recuperar_elemento_da_session(request, 'nome_do_usuario')
+    }
+    return render(request, 'index.html', dicionario)
 
 
 def cadastrar(request):
@@ -33,7 +50,6 @@ def cadastrar(request):
 '''
 Funções a partir desta parte são apenas operacionais, o controle das views fica acima
 '''
-
 # Funções para a gerencia de confeiteiros
 def seleciona_todos_os_confeiteiros_cadastrados():
     return Confeiteiro.objects.all()
@@ -88,8 +104,8 @@ def logar(request, login, senha):
     nome_do_usuario = ''
     id_do_usuario = -1
 
-    if len(Confeiteiro.objects.all().filter(login=login, Senha=senha)) == 1:
-        confeiteiro = Confeiteiro.objects.all().filter(login=login, Senha=senha)
+    if len(Confeiteiro.objects.all().filter(login=login, senha=senha)) == 1:
+        confeiteiro = Confeiteiro.objects.all().filter(login=login, senha=senha)
         logado_com_sucesso = True
         tipo_de_usuario = 'confeiteiro'
         id_do_usuario = confeiteiro[0].id
@@ -103,6 +119,7 @@ def logar(request, login, senha):
         nome_do_usuario = cliente[0].nome
 
     if logado_com_sucesso:
+        adiciona_elemento_a_session(request, 'usuario_logado', True)
         adiciona_elemento_a_session(request, 'id_do_usuario', id_do_usuario)
         adiciona_elemento_a_session(request, 'login_do_usuario', login)
         adiciona_elemento_a_session(request, 'nome_do_usuario', nome_do_usuario)
@@ -112,9 +129,9 @@ def logar(request, login, senha):
 
 
 def deslogar(request):
-    if recuperar_elemento_da_session(request, 'login'):
-        del request.session['id_do_usuario']
-        del request.session['login_do_usuario']
-        del request.session['nome_do_usuario']
-        del request.session['tipo_de_usuario']
+    request.session['usuario_logado'] = False
+    del request.session['id_do_usuario']
+    del request.session['login_do_usuario']
+    del request.session['nome_do_usuario']
+    del request.session['tipo_de_usuario']
     return redirect('/')
