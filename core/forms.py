3c1eb2bd49d7907c01021be7185cf7b25ac3443b
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-01-12 16:04:54 -0300

    Adição de novas classes de formulário ao arquivo forms.py

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,40 @@ class FormularioCadastroConfeiteiro(ModelForm):
         model = Confeiteiro
         fields = ['nome', 'login', 'senha', 'email']
         exclude = ['']
+
+
+class FormularioContatoConfeiteiro(ModelForm):
+    class Meta:
+        model = ContatoConfeiteiro
+        exclude = ['confeitero']
+
+
+class FormularioCadastroCliente(ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'login', 'Senha']
+        exclude = ['']
+
+
+class FormularioCadastroServico(ModelForm):
+    class Meta:
+        model = Postagem
+        exclude = ['confeitero']
+
+
+class FormularioContatoCliente(ModelForm):
+    class Meta:
+        model = ContatoCliente
+        exclude = ['cliente']
+
+
+class FormularioPublicacaoCliente(ModelForm):
+    class Meta:
+        model = ClientePublicacao
+        exclude = ['cliente']
+
+
+class FormularioAvalizacao(ModelForm):
+    class Meta:
+        model = Avaliacao
+        exclude = ['confeitero', 'cliente']
