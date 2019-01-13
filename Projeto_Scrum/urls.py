5081f4214dd95f4711af2c10dfb49ab93d0fd5cf
PedroHenriqueBR2 <p.lyma18@gmail.com>
2019-01-13 16:48:04 -0300

    Função de debug adicionada a url.py

--- Projeto_Scrum/urls.py
+++ Projeto_Scrum/urls.py
@@ -1,25 +1,23 @@
-"""Projeto_Scrum URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from core import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('cadastrar/', views.cadastrar, name='cadastrar'),
-path('deslogar/', views.deslogar, name='deslogar'),
+    path('deslogar/', views.deslogar, name='deslogar'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/postar/', views.nova_postagem, name='novo_servico'),
+    path('cliente/', views.cliente, name='cliente'),
+    path('cliente/postar/', views.nova_solicitacao, name='nova_solicitacao'),
 ]
+
+
+# Função para que o django mostre imagens durante o desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
